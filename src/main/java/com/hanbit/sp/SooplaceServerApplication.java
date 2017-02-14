4e71f8653832fbebffa41014a0e0f1d62ff71da5
Baek-soo-hyun <100suhyun@naver.com>
2017-02-14 11:41:56 +0900

    2017.02.14 최초 커밋

---
+++ src/main/java/com/hanbit/sp/SooplaceServerApplication.java
@@ -0,0 +1,12 @@
+package com.hanbit.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooplaceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooplaceServerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hanbit/sp/service/MemberService.java
@@ -0,0 +1,56 @@
+package com.hanbit.sp.service;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.hanbit.sp.dao.MemberDAO;
+
+@Service
+public class MemberService {
+
+	private static final String SECRET_KEY = "sooplace";
+	private PasswordEncoder passwordEncoder = new StandardPasswordEncoder(SECRET_KEY);
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	private String generateKey(String prefix) {
+		String key = prefix + StringUtils.leftPad(
+				String.valueOf(System.nanoTime()), 30, "0");
+		
+		key += StringUtils.leftPad(
+				String.valueOf((int) (Math.random() * 1000) % 100), 2, "0");
+		
+		return key;
+	}
+	
+	public String addMember(String userId, String userPw) {
+		String uid = generateKey("UID");
+		String encryptedUserPw = passwordEncoder.encode(userPw); 
+		
+		memberDAO.insertMember(uid, userId, encryptedUserPw);
+		
+		return uid;
+	}
+
+	public boolean isValidMember(String userId, String userPw) {
+		String encryptedUserPw = memberDAO.selectUserPw(userId);
+		
+		return passwordEncoder.matches(userPw, encryptedUserPw);
+	}
+
+	public void modifyMember(String uid, String userPw) {
+		String encryptedUserPw = passwordEncoder.encode(userPw);
+		
+		memberDAO.updateMember(uid, encryptedUserPw);
+	}
+
+	public String getUid(String userId) {
+		return memberDAO.selectUid(userId);
+	}
+	
+}
+
